91fb615e1cd028a60b67ccfa835ecbd3bf8b3250
stroshkov <john0205@mail.ru>
2017-08-21 19:29:53 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,43 +1,73 @@
-import csv
+documents = [{"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"}, {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"}, {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}]
+directories = {'1': ['2207 876234', '11-2'], '2': ['10006'], '3': []}
 
-flats_list = list()
+def who_by_number():
+  number = input("Скажите пожалуйста номер документа")
+  for document in documents:
+    for key, value in document.items():
+      if key == 'number' and value == number:
+        return ('Владелец этого документа', document['name'])
 
-with open('output.csv', newline='') as csvfile:
-	flats_csv = csv.reader(csvfile, delimiter=';')
-	flats_list = list(flats_csv)
+def all_date_about_document():
+  for document in documents:
+    return (document['type'], document['number'], document['name'])
 
-#можете посмотреть содержимое файлв с квартирами через print, можете - на вкладке output.csv
-#print (flats_list)
+def search_number_of_directories():
+  number = input("Скажите пожалуйста номер документа")
+  for key, value in directories.items():
+    if number in value:
+        return 'Документ на {}-ой полке'.format(key)
 
+def add_new_document():
+  type = input("Скажите пожалуйста тип документа")
+  number = input("Скажите пожалуйста номер документа")
+  name = input("Скажите пожалуйста ваше имя")
+  directory = input("Скажите пожалуйста на какую из трех полок (1, 2 или 3) его положить")
+  
+  new_document = {}
+  new_document['type'] = type
+  new_document['number'] = number
+  new_document['name'] = name
+  documents.append(new_document)
+  
+  for key, value in directories.items():
+    if directory in key:
+      directories[key].append(number)
+    else:
+      continue
+      
+  print('Дальше будет проверка добавился ли документ')
+  print('Ниже отображается список документов:')
+  print(*documents)
+  print('Ниже отображается то, какие номера документов на каких полках лежат')
+  return directories
 
-#TODO 1:
-#Напишите цикл, который проходит по всем квартирам, и печатает только новостройки
-#и их порядковые номера в файле. Подсказка:
-for i, flat in enumerate(flats_list):
-  if "новостройка" in flat:
-    print('Квартира с таким порядковым номером {0} является новостройкой'.format(i))
+def delete_document():
+  number = input("Скажите пожалуйста номер документа")
+  for document in documents:
+    for key, value in document.items():
+      if key == 'number' and value == number:
+        documents.pop(documents.index(document))
+  return ('Проверка для документов:', '\n',documents,'Проверка для полок:', '\n',directories)
+  
+def add_shelf():
+  number = input("Скажите пожалуйста номер новой полки")
+  directories.setdefault(number, [])
+  return (directories)
 
+type_of_comand = input("Введите пожалуйста команду")
 
-#TODO 2:
-#При помощи пересечения множеств попробуйте сравнить больше двух новостроек между собой одновременно
-# например, пересечение 3 и 6 квартиры из файла с ЦИАНа делается так:
-# flats_intersesction = set(flats_list[2]) & set(flats_list[5])
-# Порядковые номера новостроек вы уже получили при выполнении предыдущего задания.
-# Не забудьте вывести результат функцией print
-
-
-flats_intersesction = set(flats_list[10]) & set(flats_list[30]) & set(flats_list[66])
-print(flats_intersesction)
-
-
-# TODO3:
-#Вот так мы превратили наш массив квартир в словарь, где ключом является уникальный номер объявления,
-#а значением - ссылка на страничку с объявлением.
-# #Измените код так, чтобы стало наоборот.
-test_dict = dict()
-for i, flat in enumerate(flats_list):
-  if i == 0:
-    continue
-  test_dict[flat[len(flat)-1]] = flat[0]
-  # test_dict[flat[0]] = flat[len(flat)-1]
-print (test_dict)
+if 'p' == type_of_comand:
+  print(who_by_number())
+elif 'l' == type_of_comand:
+  print(all_date_about_document())
+elif 's' == type_of_comand:
+  print(search_number_of_directories())  
+elif 'a' == type_of_comand:
+  print(add_new_document())
+elif 'd' == type_of_comand:
+  print(delete_document()) 
+elif 'as' == type_of_comand:
+  print(add_shelf())  
+else:
+  print ("Команда введена некорректно")
