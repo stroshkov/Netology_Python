327429c5bcd9de691e9136d44482b511645e7e85
stroshkov <john0205@mail.ru>
2017-08-23 13:04:12 +0300

    Update ДЗ_2.1.py

--- ДЗ_2.1.py
+++ ДЗ_2.1.py
@@ -4,12 +4,27 @@
 # In[57]:
 
 with open('Текстовый файл для ДЗ первое второй модуль.txt', encoding = "utf8") as file:
+    content = []
     for line in file:
-        content = []
-        name = line
-        number_of_ingredients = file.readline()
-        while '|' in file.readline():
-            content.append(file.readline())
-        print('Название блюда:', name)
-        print('Количество ингредиентов:', number_of_ingredients)
-        print('Состав блюда:','\n', *content, end = '')
+        cook_book = {}
+        name = line.rstrip('\n')
+        number_of_ingredients = file.readline().rstrip('\n')
+        number_lines_with_ingredients = 0
+        
+        if '|' in file.readline():
+          number_lines_with_ingredients +=1
+        
+        while number_lines_with_ingredients > 0:
+          other_line = file.readline()
+          for word in other_line:
+            ingredients_book = {}
+            divider = other_line.find('|')
+            name_ingredient = other_line[:divider]
+            next_divider = other_line.find('||')
+            volume = other_line[divider+2:next_divider]
+            measurer = other_line[next_divider+3:len(other_line)-1]
+            number_lines_with_ingredients -=1
+            cook_book.update({'Название блюда:', name_ingredient})
+            cook_book.update({'Количество ингредиентов:', number_of_ingredients})
+            cook_book.update({'Ингредиенты:', ingredients_book})
+    print(*content)
