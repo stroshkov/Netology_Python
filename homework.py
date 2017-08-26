5b3b6fb7163fb0f0f4d420a17a85cbd9dff61100
stroshkov <john0205@mail.ru>
2017-08-27 02:41:10 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,1 +1,20 @@
-pass
+import os
+import subprocess
+
+source = 'Source'
+new_source = 'New_Source'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+folder_with_photo = os.path.join(current_dir, source)
+list_of_photo = os.listdir(folder_with_photo)
+range = list(range(5))
+
+# создадим папку куда будут отправляться новые файлы
+os.mkdir(os.path.join((current_dir, new_source)))
+
+# пройдем по всем фотографиям и дадим названи новым фотографиям в формате от 1 до 5
+for photo in list_of_photo:
+    for elements in range:
+        subprocess.run('convert.exe')
+        convert photo - resize 200 elements.jpg
+        __elements.jpg__ = os.path.join(current_dir, new_source)
+
