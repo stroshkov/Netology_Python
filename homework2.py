0942fc6c313c4fe45016f5b7698a88ca84937daa
stroshkov <john0205@mail.ru>
2017-08-28 20:08:30 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,3 +1,4 @@
+# Задание 2
 import osa
 
 with open('currencies.txt', encoding="utf8") as file:
@@ -16,11 +17,11 @@ peking_seoul_price = int(new_new_content[9])
 seoul_delhi_price = int(new_new_content[11])
 delhi_moscow = int(new_new_content[13])
 client = osa.Client('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL')
-moscow_london = client.service.Add('Content-Length:'=moscow_london_price, FromCurrency=EUR, ToCurrency=RUB)
-london_newyork = client.service.Add('Content-Length:'=london_newyork_price, FromCurrency=USD, ToCurrency=RUB)
-newyork_brasilia = client.service.Add('Content-Length:'=newyork_brasilia_price, FromCurrency=BRL, ToCurrency=RUB)
-brasilia_peking = client.service.Add('Content-Length:'=brasilia_peking_price, FromCurrency=CNY, ToCurrency=RUB)
-peking_seoul = client.service.Add('Content-Length:'=peking_seoul_price, FromCurrency=KRW, ToCurrency=RUB)
+moscow_london = client.service.ConversionRate('Content-Length:'=moscow_london_price, FromCurrency=EUR, ToCurrency=RUB)
+london_newyork = client.service.ConversionRate('Content-Length:'=london_newyork_price, FromCurrency=USD, ToCurrency=RUB)
+newyork_brasilia = client.service.ConversionRate('Content-Length:'=newyork_brasilia_price, FromCurrency=BRL, ToCurrency=RUB)
+brasilia_peking = client.service.ConversionRate('Content-Length:'=brasilia_peking_price, FromCurrency=CNY, ToCurrency=RUB)
+peking_seoul = client.service.ConversionRate('Content-Length:'=peking_seoul_price, FromCurrency=KRW, ToCurrency=RUB)
 seoul_delhi = client.service.ConversionRate ('Content-Length:'=seoul_delhi_price, FromCurrency=INR, ToCurrency=RUB)
 result = moscow_london + london_newyork + newyork_brasilia + brasilia_peking + peking_seoul + seoul_delhi + delhi_moscow
 print(round(result, 0))
