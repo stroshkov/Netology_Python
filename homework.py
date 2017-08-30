05984b11c182aa800e2a51deff12ea337e433aca
stroshkov <john0205@mail.ru>
2017-08-30 12:34:20 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 
 source = 'Source'
-new_source = 'New_Source'
+program = 'convert.exe'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 folder_with_photo = os.path.join(current_dir, source)
 list_of_photo = os.listdir(folder_with_photo)
-range = list(range(5))
+program_path = os.path.join(current_dir, program)
 
 # создадим папку куда будут отправляться новые файлы
 os.mkdir(os.path.join((current_dir, new_source)))
@@ -15,4 +15,3 @@ os.mkdir(os.path.join((current_dir, new_source)))
 for photo in list_of_photo:
     args = [program_path, file, '-resize', '200', file]
     proc = Popen(args)
-    
