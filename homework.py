98e32409feaabd0f0570899607901c5b05804c24
stroshkov <john0205@mail.ru>
2017-08-31 16:45:27 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,17 +1,22 @@
 import os
 import subprocess
 
-source = 'Source'
-program = 'convert.exe'
-current_dir = os.path.dirname(os.path.abspath(__file__))
-folder_with_photo = os.path.join(current_dir, source)
-list_of_photo = os.listdir(folder_with_photo)
-program_path = os.path.join(current_dir, program)
+def photo_converter(photo_for_resize):
+    print('Идет обработка фотографии {}'.format(photo_for_resize))
+    subprocess.call('convert {} -resize 200 {}'.format(source_file, result_file))
+    print('Обработка фотографии {} завершена'.format(photo_for_resize))
 
-# создадим папку куда будут отправляться новые файлы
-os.mkdir(os.path.join((current_dir, new_source)))
-
-# пройдем по всем фотографиям и дадим названи новым фотографиям в формате от 1 до 5
-for photo in list_of_photo:
-    args = [program_path, file, '-resize', '200', file]
-    proc = Popen(args)
+if __name__ == '__main__':
+    source = 'Source'
+    program = 'convert.exe'
+    new_source = 'New_Source'
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    source_path = os.path.join(current_dir, source)
+    list_of_photo = os.listdir(source_path)
+    result_path = os.path.join(current_dir, new_source)
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+    for photo in list_of_photo:
+        source_file = os.path.join(source_path, photo)
+        result_file = os.path.join(result_path, photo)
+        photo_converter(photo)
