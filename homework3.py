b64167fa4129294fb8a707baa306e80a9948250e
stroshkov <john0205@mail.ru>
2017-09-01 17:53:39 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,22 +1,29 @@
 # Задание 3
 import osa
 
+
+def convert_distance(distance):
+    client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
+    return client.service.ChangeLengthUnit(
+        LengthValue=distance,
+        fromLengthUnit='Miles',
+        toLengthUnit='Kilometers'
+    )
+
+
+def calculation(file_for_calculation):
+    distance_of_trip = 0
+
+    for line in file_for_calculation:
+        content = line.split(' ')
+        length = content[1]
+        clear_length = length.replace(',', '')
+        length_for_calculation = float(clear_length)
+        distance_of_trip += length_for_calculation
+
+    distance_of_trip_in_km = convert_distance(distance_of_trip)
+    return distance_of_trip_in_km
+
+
 with open('travel.txt', encoding="utf8") as file:
-    content = []
-
-    for line in file:
-        content.append(line)
-
-new_content = ''.join(content)
-new_new_content = new_content.split(' ')
-moscow_london_length = float(new_new_content[1])
-london_newyork_length = float(new_new_content[3])
-newyork_brasilia_length = float(new_new_content[5])
-brasilia_peking_length = float(new_new_content[7])
-peking_seoul_length = float(new_new_content[9])
-seoul_delhi_length = float(new_new_content[11])
-delhi_moscow_length = float(new_new_content[13])
-sum_length = moscow_london_length + london_newyork_length + newyork_brasilia_length + brasilia_peking_length + peking_seoul_length + seoul_delhi_length + delhi_moscow_length
-client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
-result = client.service.ChangeLengthUnit('Content-Length:'=sum_length, fromLengthUnit=Mils, toLengthUnit=Kilometers)
-print(round(result, 2))
+    print(round(calculation(file), 2), 'Miles')
